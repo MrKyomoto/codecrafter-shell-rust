fcd8dd04cb3782dc68c4663eb94f4243c0a2b2b0
kyomoto-pixelbook <2028566723@qq.com>
2025-11-04 16:12:19 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-const BUILD_IN_COMMANDS: [&str; 1] = ["exit"];
+const BUILD_IN_COMMANDS: [&str; 2] = ["exit", "echo"];
 enum Command {
-    ExitCommand { exit_code: i32 },
+    CommandBuiltIn { builtin_command: BuiltInCommand },
     CommandNotFound { unknown_command_name: String },
+}
+enum BuiltInCommand {
+    Exit { exit_code: i32 },
+    Echo { content: String },
 }
 
 trait ParseInput {
@@ -14,12 +18,35 @@ trait ParseInput {
 impl ParseInput for Command {
     fn from_input(input: &str) -> Self {
         let input = input.trim();
-        if input.starts_with("exit ") {
-            let args = input.split_whitespace().collect::<Vec<&str>>();
-            return Command::ExitCommand {
-                exit_code: args[1].parse().unwrap(),
-            };
+        let args = input.split_whitespace().collect::<Vec<&str>>();
+        // NOTE: builtin command
+        for builtin in BUILD_IN_COMMANDS {
+            if args[0] == builtin {
+                match builtin {
+                    "exit" => {
+                        return Command::CommandBuiltIn {
+                            builtin_command: BuiltInCommand::Exit {
+                                exit_code: if args.len() > 1 {
+                                    args[1].parse().unwrap_or_default()
+                                } else {
+                                    0
+                                },
+                            },
+                        }
+                    }
+                    "echo" => {
+                        return Command::CommandBuiltIn {
+                            builtin_command: BuiltInCommand::Echo {
+                                content: String::from(input.trim_start_matches("echo ")),
+                            },
+                        }
+                    }
+                    _ => {}
+                }
+            }
         }
+
+        // NOTE: other command
         match input {
             _ => {
                 return Command::CommandNotFound {
@@ -39,14 +66,19 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
         let command = Command::from_input(&input);
         match command {
-            Command::ExitCommand { exit_code } => {
-                break;
-            }
             Command::CommandNotFound {
                 unknown_command_name,
             } => {
                 println!("{}: command not found", unknown_command_name);
             }
+            Command::CommandBuiltIn { builtin_command } => match builtin_command {
+                BuiltInCommand::Exit { exit_code: _ } => {
+                    break;
+                }
+                BuiltInCommand::Echo { content } => {
+                    println!("{}", content);
+                }
+            },
         }
     }
 }
